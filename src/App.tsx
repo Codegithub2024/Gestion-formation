43698f5b31fdfe060e97a8d94c69def3ce66263f
BITIE Carol <154431730+Codegithub2024@users.noreply.github.com>
2026-04-03 01:04:30 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-violet-400 p-5">
+        <h2 className="text-2xl font-bold text-white">
+          Welcome to this website
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
